392752bd7748f316013d9ce1af06eab74741a6d2
GROS Christian <christian-gros@outlook.fr>
2022-06-21 12:06:24 +0200

    nice cooc

--- src/components/StreamsCard.js
+++ src/components/StreamsCard.js
@@ -1,4 +1,10 @@
 import { useState, useEffect } from "react";
+import {
+  TwitchEmbed,
+  TwitchChat,
+  TwitchClip,
+  TwitchPlayer,
+} from "react-twitch-embed";
 
 const StreamsCard = () => {
   const [respons, setResponse] = useState([{}]);
@@ -7,7 +13,7 @@ const StreamsCard = () => {
 
   const fetchParties = (url) => {
     const headers = {
-      "Authorization": "Bearer j66dvirbretd668on4gxvr643jnqpa",
+      Authorization: "Bearer j66dvirbretd668on4gxvr643jnqpa",
       "Client-Id": "lb8o6spujvoj8d88sbcidt49yjp5gj",
     };
     fetch(url, { headers })
@@ -16,7 +22,11 @@ const StreamsCard = () => {
         let tempList = data.data;
         setResponse(data.data);
         console.log(tempList);
-        setImg(tempList.map((rep) => (rep.box_art_url.replace("{width}", "300").replace("{height}", "300"))))
+        setImg(
+          tempList.map((rep) =>
+            rep.box_art_url.replace("{width}", "300").replace("{height}", "300")
+          )
+        );
         // console.log(data.data)
       })
       .catch((err) => console.log(err));
@@ -34,9 +44,23 @@ const StreamsCard = () => {
         <li>{rep.name}</li>
       ))}
       {img.map((rep) => (
-        <img src={rep}
-        />
+        <img src={rep} />
       ))}
+      <div>
+        <TwitchEmbed
+          channel="smoke"
+          id="Smoke"
+          theme="dark"
+          muted
+          onVideoPause={() => console.log(":(")}
+        />
+        <TwitchChat channel="moonstar_x" theme="dark" />
+        <TwitchClip
+          clip="WealthyBumblingKimchiItsBoshyTime"
+          parent={["mycoolsite.com, anotherawesomesite.net"]}
+        />
+        <TwitchPlayer video="333014765" />
+      </div>
     </div>
   );
 };
